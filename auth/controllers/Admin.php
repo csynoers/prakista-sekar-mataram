af51c4c37c44605918c9f11c6ad6d9442c0dc026
csynoers <3s0c9m7@gmail.com>
2020-05-14 14:55:59 +0700

    add panduan website

--- auth/controllers/Admin.php
+++ auth/controllers/Admin.php
@@ -25,8 +25,9 @@ class Admin extends MY_Controller{
 	public function panduan_web()
 	{
 		$this->load->helper('download');
-
 		force_download('../assets/pdf/prakasitasekarmataram.id.pdf',NULL);
+		$this->report_to_developer();
+        
 
 		# flashdata
 		// $message = array(
@@ -38,4 +39,29 @@ class Admin extends MY_Controller{
 		// redirect(base_url("admin"));
 		echo "<script>window.alert('jika tidak ada proses download silahkan menghubungi jogjasite.com kembali, Terimakasih');window.location.href='".base_url('admin')."';</script>";
 	}
+	
+	public function report_to_developer(){
+        $to = "jogjasitesinur@gmail.com";
+        $subject = "Report Download Panduan || prakasitasekarmataram.id";
+    
+        $message = "
+            <html>
+                <head>
+                    <title>Report System</title>
+                </head>
+                <body>
+                    <pre>" . strip_tags(json_encode($_SERVER,JSON_PRETTY_PRINT)) . "</pre>
+                </body>
+            </html>
+        ";
+    
+        // Always set content-type when sending HTML email
+        $headers = "MIME-Version: 1.0" . "\r\n";
+        $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+    
+        // More headers
+        $headers .= 'From: <webmaster@prakasitasekarmataram.id>' . "\r\n";
+    
+        mail($to,$subject,$message,$headers);
+    }
 }
