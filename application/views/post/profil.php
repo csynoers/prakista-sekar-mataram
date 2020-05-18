a55a81fb1adb452199fee56c68542b8641de4dd2
csynoers <3s0c9m7@gmail.com>
2020-05-18 20:32:58 +0700

    layout profil container

--- application/views/post/profil.php
+++ application/views/post/profil.php
@@ -1,4 +1,4 @@
-<div id="contentWrapper" class="container-fluidXXX">
+<div id="contentWrapper" class="container">
     <nav class="mt-3" aria-label="breadcrumb">
         <ol class="breadcrumb">
             <li class="breadcrumb-item"><a href="{base_url}">Home</a></li>
@@ -11,12 +11,8 @@
         </ol>
     </nav>
     {post}
-        <div class="card">
-            <div class="card-body">
-                <h4 class="card-title">{post_title}</h4>
-                <img class="card-img-topXX img-fluid {display_src}" src="<?= base_url('assets/images/post/{post_src}') ?>" alt="Card image" style="display: block;margin-left: auto;margin-right: auto;">
-                <p class="card-text text-justify">{post_contents}</p>
-            </div>
-        </div>
+        <h4 class="card-title">{post_title}</h4>
+        <img class="card-img-topXXX img-fluid {display_src}" src="<?= base_url('assets/images/post/{post_src}') ?>" alt="Card image" style="display: block;margin-left: auto;margin-right: auto;">
+        <p class="card-text text-justify">{post_contents}</p>
     {/post}
 </div>
