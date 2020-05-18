046eba7232529a1647869dc0b531aa6f4bc0a9a0
csynoers <3s0c9m7@gmail.com>
2020-05-18 14:38:27 +0700

    remove no scrolling

--- application/views/footer.php
+++ application/views/footer.php
@@ -1,4 +1,4 @@
-    </div>
+</div>
   </div>
   <footer>
     <div class="container">
@@ -27,47 +27,48 @@
   <script type="text/javascript">
       (function(j){
         /* set full screen no scrolling */
-        if ( screen.availWidth > 767 ) { /* desktop view */
-          j('body').attr( {
-            "width" : screen.availWidth,
-            "height" : screen.availHeight,
-            "style" : "overflow-y: hidden;"
-          } );
+        // if ( screen.availWidth > 767 ) { /* desktop view */
+        //   j('body').attr( {
+        //     "width" : screen.availWidth,
+        //     "height" : screen.availHeight,
+        //     "style" : "overflow-y: hidden;"
+        //   } );
           
-          j('#contentWrapper').attr( {
-            "style" : `height: calc( calc(${screen.availHeight}px/100)*65) !important;overflow: auto;`
-          } );
+        //   j('#contentWrapper').attr( {
+        //     "style" : `height: calc( calc(${screen.availHeight}px/100)*65) !important;overflow: auto;`
+        //   } );
 
-          j.each(j('.carousel-item'),function(){
-            let img_src = j(this).data('bg-src');
-            j(this).attr({"style":`
-              height: calc( calc(${screen.availHeight}px/100)*62) !important;
-              background-image: url('${img_src}');
-              background-size: cover;
-              background-position: center;
-            `});
-          })
+        //   j.each(j('.carousel-item'),function(){
+        //     let img_src = j(this).data('bg-src');
+        //     j(this).attr({"style":`
+        //       height: calc( calc(${screen.availHeight}px/100)*62) !important;
+        //       background-image: url('${img_src}');
+        //       background-size: cover;
+        //       background-position: center;
+        //     `});
+        //   })
 
-        } else { /* mobile view */
-          j('#navPrimary').addClass('sticky-top');
-          j.each(j('.carousel-item'),function(){
-            let img_src = j(this).data('bg-src');
-            j(this).attr({"style":`
-              height: calc( calc(${screen.availHeight}px/100)*50) !important;
-              background-image: url('${img_src}');
-              background-size: cover;
-              background-position: center;
-            `});
-          })
+        // } else { /* mobile view */
+        //   j('#navPrimary').addClass('sticky-top');
+        //   j.each(j('.carousel-item'),function(){
+        //     let img_src = j(this).data('bg-src');
+        //     j(this).attr({"style":`
+        //       height: calc( calc(${screen.availHeight}px/100)*50) !important;
+        //       background-image: url('${img_src}');
+        //       background-size: cover;
+        //       background-position: center;
+        //     `});
+        //   })
 
-        }
+        // }
 
         /* carousel setting */
-        var li,items;
-        li = j('.carousel-indicators').find('li');
-        items= j('.carousel-item');
-        j(li[0]).addClass('active');
-        j(items[0]).addClass('active');
+        // var li,items;
+        // li = j('.carousel-indicators').find('li');
+        // items= j('.carousel-item');
+        // j(li[0]).addClass('active');
+        // j(items[0]).addClass('active');
+        
         // items.on('click',function(){
         //   if ( j(this).attr('data-link'!='') )
         //   {
